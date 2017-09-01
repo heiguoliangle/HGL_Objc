c91bad390f83426919b7211d73c33be691992c80
heiguoliangle <heiguoliangle@163.com>
2017-09-01 10:15:37 +0800

    配置初始化项目

--- Example/HGL_Objc/HGLViewController.m
+++ Example/HGL_Objc/HGLViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "HGLViewController.h"
+#import "HGLDataSouce.h"
 
-@interface HGLViewController ()
-
+@interface HGLViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,weak)UITableView * tableView;
+@property(nonatomic,strong)HGLDataSouce * dataSouce;
 @end
 
 @implementation HGLViewController
@@ -17,7 +19,70 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupUINav];
+    [self setupTableView];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)setupUINav {
+    
+    UIBarButtonItem * item = [[UIBarButtonItem alloc]initWithTitle:@"reload" style:UIBarButtonItemStyleDone target:self action:@selector(reloadView)];
+//    self.navigationController.navigationItem.rightBarButtonItem = item;
+    self.navigationItem.rightBarButtonItem = item;
+    
+}
+
+- (void)reloadView {
+    
+    NSMutableArray * mutArr  = [NSMutableArray array];
+    for (int i = 9; i < 99; i ++) {
+        HGLDataSourceModel * model = [[HGLDataSourceModel alloc]init];
+        model.title = [NSString stringWithFormat:@"%d",i];
+        [mutArr addObject:model];
+    }
+    self.dataSouce.dataArr = mutArr.copy;
+    [self.tableView reloadData];
+}
+
+#pragma mark - setTableView
+- (void)setupTableView {
+    
+    
+    setCellModel modelBlock = ^(HGLDataSourceCell * cell,HGLDataSourceModel * model){
+        [cell setCellDataWith:model];
+    };
+    
+    HGLDataSourceModel * model1 = [[HGLDataSourceModel alloc]init];
+    model1.title = @"1";
+    HGLDataSourceModel * model2 = [[HGLDataSourceModel alloc]init];
+    model2.title = @"2";
+    NSArray * cellArr = @[model1,model2];
+    
+    static NSString * key = @"cellIdentifier";
+    HGLDataSouce * dataSouce = [[HGLDataSouce alloc]initWithDataArr:cellArr cellIdentifier:key finishBlock:modelBlock];
+    
+    UITableView * tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    tableView.dataSource = dataSouce;
+    tableView.delegate = self;
+    self.dataSouce = dataSouce;
+    [self.view addSubview:tableView];
+    self.tableView = tableView;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 100;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 10 ;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 0.001;
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Example/Pods/Target Support Files/HGL_Objc/HGL_Objc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HGL_Objc : NSObject
+@end
+@implementation PodsDummy_HGL_Objc
+@end
